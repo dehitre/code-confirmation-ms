8ca5bd7a5f3a4f86516ea022beebd4603f7c5da9
kathie <esmirnova@okko.tv>
2019-09-21 21:09:39 +0300

    add checking in db

--- test_ccp.py
+++ test_ccp.py
@@ -3,53 +3,73 @@ import json
 import pytest
 import random
 import psycopg2
+import allure_pytest
+import allure
 
 
-@pytest.fixture(scope="module")
+
+@pytest.fixture(scope="function")
 def get_address():
     link = "http://confirmation-code-processor.pre.spb.play.dc/v1/codes"
     return link
 
-@pytest.fixture(scope="module")
+
+@pytest.fixture(scope="function")
 def get_phone():
     return int("7999" + str(random.randint(1000000, 9999999)))
 
-@pytest.fixture(scope="module")
+
+@pytest.fixture(scope="function")
 def get_user():
     return random.randint(100000, 999999)
 
-@pytest.fixture(scope="module")
+
+@pytest.fixture(scope="function")
 def use_by_phone_address():
     def _verification_address(code):
         link = "http://confirmation-code-processor.pre.spb.play.dc/v1/codes/" + code + "/check-phone-and-delete"
         return link
     return _verification_address
 
-@pytest.fixture(scope="module")
+
+@pytest.fixture(scope="function")
 def use_by_user_address():
     def _verification_address(code):
         link = "http://confirmation-code-processor.pre.spb.play.dc/v1/codes/" + code + "/user"
         return link
     return _verification_address
 
-@pytest.fixture(scope="module")
-def change_time_of_code():
-    def _change_time(code):
-        query = "UPDATE confirmationcode set created_ts = now()-INTERVAL '30 minutes' where code='" + code + "'"
 
-        conn = psycopg2.connect(host="pre-postgres-confirmationcodes-master.spb.play.dc", port="5433",
-                                dbname="confirmationcodes", user="confirmationcodes", password="confirmationcodes")
-        cursor = conn.cursor()
-        cursor.execute(query)
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return  True
-    return _change_time
+@allure.step("Change code creation time in db")
+def change_time_of_code(code):
+    query = "UPDATE confirmationcode set created_ts = now()-INTERVAL '30 minutes' where code='" + code + "'"
 
+    conn = psycopg2.connect(host="pre-postgres-confirmationcodes-master.spb.play.dc", port="5433",
+                            dbname="confirmationcodes", user="confirmationcodes", password="confirmationcodes")
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return True
 
 
 
+@allure.step("Find code in db")
+def check_code(code):
+    query = "SELECT code FROM confirmationcode where code='" + code + "'"
+    conn = psycopg2.connect(host="pre-postgres-confirmationcodes-master.spb.play.dc", port="5433",
+                            dbname="confirmationcodes", user="confirmationcodes", password="confirmationcodes")
+    cursor = conn.cursor()
+    result = cursor.execute(query)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return result
+
+
+
+@allure.title("Check creation and using confirmation code")
 def test_code_confirmation(get_address, get_phone,use_by_phone_address):
     data = {"codeType": "CONFIRMATION", "phone": get_phone, "requestType": "BY_PHONE"}
     headers = {'Content-Type': 'application/json'}
@@ -62,8 +82,11 @@ def test_code_confirmation(get_address, get_phone,use_by_phone_address):
     assert code_ttl =="PT4H"
     use_code_response = requests.post(use_by_phone_address(code), headers=headers, data=json.dumps(data))
     assert use_code_response.status_code == 200
+    code_in_db = check_code(code)
+    assert code_in_db is None
 
 
+@allure.title("Check creation and using registration code")
 def test_create_code_registration(get_address, get_phone,use_by_phone_address):
     data = {"codeType": "REGISTRATION", "phone": get_phone, "requestType": "BY_PHONE"}
     headers = {'Content-Type': 'application/json'}
@@ -76,7 +99,11 @@ def test_create_code_registration(get_address, get_phone,use_by_phone_address):
     assert code_ttl =="PT30M"
     use_code_response = requests.post(use_by_phone_address(code), headers=headers, data=json.dumps(data))
     assert use_code_response.status_code == 200
+    code_in_db = check_code(code)
+    assert code_in_db is None
 
+
+@allure.title("Check creation and using merge code")
 def test_create_code_merge(get_address, get_user,use_by_user_address):
     data = {"codeType": "MERGE", "userId": get_user, "requestType": "BY_USER_ID"}
     headers = {'Content-Type': 'application/json'}
@@ -90,8 +117,11 @@ def test_create_code_merge(get_address, get_user,use_by_user_address):
     use_code_response = requests.post(use_by_user_address(code), headers=headers, data=json.dumps(data))
     assert use_code_response.status_code == 200
     assert use_code_response.json()["userId"] == data["userId"]
+    code_in_db = check_code(code)
+    assert code_in_db is None
 
 
+@allure.title("Check wrong code for confirmation/registration codes")
 def test_wrong_code_error_by_phone(use_by_phone_address, get_phone):
     data = {"codeType": "REGISTRATION", "phone": get_phone, "requestType": "BY_PHONE"}
     headers = {'Content-Type': 'application/json'}
@@ -101,7 +131,8 @@ def test_wrong_code_error_by_phone(use_by_phone_address, get_phone):
     assert use_code_response.json()["errorCode"] == "INCORRECT_CODE"
 
 
-def test_expired_code_error_by_phone(get_address,use_by_phone_address, get_phone, change_time_of_code):
+@allure.title("Check expired code for confirmation/registration codes")
+def test_expired_code_error_by_phone(get_address,use_by_phone_address, get_phone):
     data = {"codeType": "REGISTRATION", "phone": get_phone, "requestType": "BY_PHONE"}
     headers = {'Content-Type': 'application/json'}
     create_code_response = requests.post(get_address, headers=headers, data=json.dumps(data))
@@ -112,7 +143,9 @@ def test_expired_code_error_by_phone(get_address,use_by_phone_address, get_phone
     assert use_code_response.json()["message"] == "Confirmation code expired"
     assert use_code_response.json()["errorCode"] == "CODE_EXPIRED"
 
-def test_wrong_code_error_by_user( get_user, use_by_user_address):
+
+@allure.title("Check wrong code for merge codes")
+def test_wrong_code_error_by_user(get_user, use_by_user_address):
     data = {"codeType": "MERGE", "userId": get_user, "requestType": "BY_USER_ID"}
     headers = {'Content-Type': 'application/json'}
     use_code_response = requests.post(use_by_user_address("0000"), headers=headers, data=json.dumps(data))
@@ -121,7 +154,8 @@ def test_wrong_code_error_by_user( get_user, use_by_user_address):
     assert use_code_response.json()["errorCode"] == "INCORRECT_CODE"
 
 
-def test_expired_code_error_by_user(get_address, get_user,use_by_user_address,change_time_of_code):
+@allure.title("Check expired code for merge codes")
+def test_expired_code_error_by_user(get_address, get_user, use_by_user_address):
     data = {"codeType": "MERGE", "userId": get_user, "requestType": "BY_USER_ID"}
     headers = {'Content-Type': 'application/json'}
     create_code_response = requests.post(get_address, headers=headers, data=json.dumps(data))
